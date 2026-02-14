f9fca633c1a1c96c8252dba09897b0a89d96aff8
praveenraj2023-jes <praveenmtechse2023@gmail.com>
2026-02-14 16:01:48 +0530

    Java With Mathematical operation

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Jenkins! This is praveen raj doing an automatic build creating an scm poll ");
+        int a = 10;
+        int b = 5;
+        int result = a + b;
+        System.out.println("Mathematical Operation (10 + 5) = " + result);
     }
 }
